de8d23cb5dc907fc81b140f002c590e61fc1762f
Gonjou <ameliamarteguzman08@gmail.com>
2026-04-18 12:06:30 -0400

    Added some docstrings

--- apod/client.py
+++ apod/client.py
@@ -3,17 +3,42 @@ from . import config
 
 
 class APODClient:
-    # Initialize url, api_key and timeout instance variables.
+    """
+    Client that fetchs NASA's Astronomical Picture of the Day.
+    It handles requests with automatic session management and error handling.
+
+    """
+
     def __init__(self, base_url=config.BASE_URL, api_key=config.API_KEY, timeout=config.TIMEOUT):
+        """
+        Initialize APODClient.
+
+        Args:
+        base_url = url of the APOD website
+        api_key = your API key gotten from your environment variable "NASA_API_KEY"
+        timeout = Request timeout in secs. Default is set to 10
+
+        """
 
         self.base_url = base_url
         self.api_key = api_key
         self.timeout = timeout
 
-        self.session = requests.Session() # Creating a session.
+        """Creating a session"""
+        self.session = requests.Session() 
 
-    # Send request.
     def _get(self, params):
+        """Sends request. 
+
+        Args:
+        params = query parameters, such as {"date": "2026-04-16"}
+
+        Returns:
+        Parsed JSON response from the API
+
+        Raises:
+
+        """
         
         params = params.copy()
         params["api_key"] = self.api_key
